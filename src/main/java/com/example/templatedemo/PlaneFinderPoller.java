62b6c19cf4a1c2a3e7fd598839e0ca9755cb17c3
Ashish Sharda <ashishjsharda@gmail.com>
2021-10-23 14:58:14 -0400

    Add all my files

--- src/main/java/com/example/templatedemo/PlaneFinderPoller.java
+++ src/main/java/com/example/templatedemo/PlaneFinderPoller.java
@@ -3,6 +3,7 @@ package com.example.templatedemo;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisOperations;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 
@@ -18,5 +19,13 @@ public class PlaneFinderPoller {
 
         this.redisConnectionFactory = redisConnectionFactory;
         this.redisOperations = redisOperations;
+    }
+    @Scheduled(fixedRate = 1000)
+    private void pollPlanes(){
+        redisConnectionFactory.getConnection().serverCommands().flushDb();
+        webClient.get().retrieve().bodyToFlux(Aircraft.class)
+                .filter(plane->!plane.getReg().isEmpty()).toStream().forEach(ac->redisOperations.opsForValue().set(ac.getReg(),ac));
+        redisOperations.opsForValue().getOperations()
+                .keys("*").forEach(ac->System.out.println(redisOperations.opsForValue()));
     }
 }
