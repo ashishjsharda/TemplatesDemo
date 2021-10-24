f761b4c94ea3e99d97f965d7b87a1987480b0dc6
Ashish Sharda <ashishjsharda@gmail.com>
2021-10-24 09:23:31 -0400

    Add all my files

--- src/main/java/com/example/templatedemo/Aircraft.java
+++ src/main/java/com/example/templatedemo/Aircraft.java
@@ -5,11 +5,14 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.redis.core.RedisHash;
+
 import java.time.Instant;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@RedisHash
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Aircraft {
     @Id
